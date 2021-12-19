f8d39b88553aa7674833c0832f0709bab8d94f71
Emin Kazım <eminjk@code.edu.az>
2021-12-19 13:02:43 -0800

    Custom List

---
+++ Indexer/Indexer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Indexer.Models;
+using System;
+
+namespace Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            MyList<object> list = new MyList<object>();
+            list.Add("Phyton");
+            list.Add("Java Sctript");
+            list.Add(26);
+            list.Add("C#");
+
+            list.AddRange(22);
+
+            //list.Clear(list);
+
+
+            //list.FindList("C#");
+
+
+
+        }
+    }
+}
